256c6ee3c7d591243110dd3550a1830ccc65be85
MOJ <mojoblanco@gmail.com>
2020-04-13 21:33:32 +0100

    Add validation

--- TipCalculator/Models/TipModel.cs
+++ TipCalculator/Models/TipModel.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TipCalculator.Models
 {
     public class TipModel
-    {
+    { 
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Amount cannot be zero or negative")]
         public double Amount { get; set; }
 
+        [Required]
         public int Percentage { get; set; }
 
+        [Required]
+        [Range(1, 100, ErrorMessage = "Invalid number of people")]
         public int PeopleCount { get; set; }
     }
 }
