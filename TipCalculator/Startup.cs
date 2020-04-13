a083a0d8fbe809abe0f9f10c796c404334c1d838
MOJ <mojoblanco@gmail.com>
2020-04-13 21:00:07 +0100

    Clean project

--- TipCalculator/Startup.cs
+++ TipCalculator/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TipCalculator.Data;
 
 namespace TipCalculator
 {
@@ -28,7 +27,6 @@ namespace TipCalculator
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
