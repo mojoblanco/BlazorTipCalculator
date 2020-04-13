83e66ab33a4def5f29c3dec7360f1fdfc116a5e6
MOJ <mojoblanco@gmail.com>
2020-04-13 21:23:21 +0100

    Design the form

---
+++ TipCalculator/Models/TipModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TipCalculator.Models
+{
+    public class TipModel
+    {
+        public double Amount { get; set; }
+
+        public int Percentage { get; set; }
+
+        public int PeopleCount { get; set; }
+    }
+}
